ca6ad3478af9c33fd9cdb81de8019ecbb1c6515b
Falkreon <falkreon@gmail.com>
2018-12-24 22:10:01 -0600

    Diagnose and repair sounds

--- src/main/java/com/elytradev/engination/Engination.java
+++ src/main/java/com/elytradev/engination/Engination.java
@@ -8,12 +8,16 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Engination implements ModInitializer {
-	public static SoundEvent SOUND_JUMP = new SoundEvent(new Identifier("engination", "launcher_activate"));
-	public static SoundEvent SOUND_THROW = new SoundEvent(new Identifier("engination", "projectile_throw"));
-	public static SoundEvent SOUND_SQUISH = new SoundEvent(new Identifier("engination", "squish"));
+	public static SoundEvent SOUND_JUMP;
+	public static SoundEvent SOUND_THROW;
+	public static SoundEvent SOUND_SQUISH;
 	
 	@Override
 	public void onInitialize() {
+		SOUND_JUMP =  new SoundEvent(new Identifier("engination", "launcher_activate"));
+		SOUND_THROW =  new SoundEvent(new Identifier("engination", "projectile_throw"));
+		SOUND_SQUISH = new SoundEvent(new Identifier("engination", "squish"));
+		
 		Registry.register(Registry.SOUND_EVENT, "engination:launcher_activate", SOUND_JUMP);
 		Registry.register(Registry.SOUND_EVENT, "engination:projectile_throw",  SOUND_THROW);
 		Registry.register(Registry.SOUND_EVENT, "engination:squish",            SOUND_SQUISH);
