b191537e8d87efc585ba4cc2f9cb2bc7a033e972
Falkreon <falkreon@gmail.com>
2019-06-27 18:15:16 -0500

    Fix Landing Pad dropping nothing (closes #2)

--- src/main/java/com/elytradev/engination/block/LandingPadBlock.java
+++ src/main/java/com/elytradev/engination/block/LandingPadBlock.java
@@ -1,11 +1,17 @@
 package com.elytradev.engination.block;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.loot.context.LootContext;
 
 public class LandingPadBlock extends Block {
 
@@ -18,4 +24,13 @@ public class LandingPadBlock extends Block {
 		var3.fallDistance = 0;
 	}
 
+	@Override
+	public List<ItemStack> getDroppedStacks(BlockState blockState_1, LootContext.Builder lootContext$Builder_1) {
+		List<ItemStack> superStacks = super.getDroppedStacks(blockState_1, lootContext$Builder_1);
+		if (!superStacks.isEmpty()) return superStacks;
+		
+		ArrayList<ItemStack> result = new ArrayList<>();
+		result.add(new ItemStack(this, 1));
+		return result;
+	}
 }
